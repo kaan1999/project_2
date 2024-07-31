1f8bbb401d013d648c9cd8fe258db975cb141459
Ahmet Kaan <ahmet.kaan.yalcinkaya99@gmail.com>
2024-07-31 17:39:40 +0300

    change main page by kaan

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Archi's Academy Github Kursuna Hoşgeldiniz!
+          Git & Github Öğreniyorum!
         </p>
         <a
           className="App-link"
